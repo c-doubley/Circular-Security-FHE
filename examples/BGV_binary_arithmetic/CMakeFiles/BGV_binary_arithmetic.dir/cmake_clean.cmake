a3d8b6c03ab4d7995c35f269d9e1e12a5d7fd5e3
PaperReviewww <3111765384@qq.com>
2023-02-24 22:06:40 +0800

    first upload

---
+++ examples/BGV_binary_arithmetic/CMakeFiles/BGV_binary_arithmetic.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BGV_binary_arithmetic.dir/BGV_binary_arithmetic.cpp.o"
+  "BGV_binary_arithmetic.pdb"
+  "BGV_binary_arithmetic"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BGV_binary_arithmetic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ examples/BGV_packed_arithmetic/CMakeFiles/BGV_packed_arithmetic.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BGV_packed_arithmetic.dir/BGV_packed_arithmetic.cpp.o"
+  "BGV_packed_arithmetic.pdb"
+  "BGV_packed_arithmetic"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BGV_packed_arithmetic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
